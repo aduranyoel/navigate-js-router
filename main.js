47e106bb3e781709b48e55a7be35b62cf9b0d72b
Tony Duran <yoet92@gmail.com>
2019-08-29 17:48:30 -0300

    modulo

--- main.js
+++ main.js
@@ -1,12 +1,84 @@
+var navigate = (function(){
 
-var Router = function(name, routes){
-    return {
-        name: name,
-        routes: routes
+    function setRoutes(arrObj){
+        this.routes = new Router(arrObj);
     }
-}
+    function getRoutes(){
+        return this.routes;
+    }
+    var Router = function(paramRouter){
+        return {
+            routes: paramRouter
+        }
+    }
+    function extractRoute(route){
+        return route.slice(route.indexOf('#')+1);
+    }
+    function getViews(){
+        var views = document.querySelectorAll('[data-route-view]');
+        var arrayViews = [];
+        for (var i = 0, len = views.length ; i < len ; i++){
+            arrayViews.push(views[i])
+        }
+        return arrayViews;
+    }
+    function getActiveRoutes(){
+        var activeRoutes = document.querySelectorAll('[data-route]');
+        var arrayActiveRoutes = [];
+        for (var i = 0, len = activeRoutes.length ; i < len ; i++){
+            arrayActiveRoutes.push(views[i])
+        }
+        return arrayActiveRoutes;
+    }
+    function paraEvento(e){
+        goTo(e.target.getAttribute('data-route'))
+    }
+    function goTo(toRoute, opt){
+        var opt = opt || {};
+        opt.routes = opt.routes || [];
+        opt.trace = typeof opt.trace === "undefined" ? true : opt.trace
+        var route = toRoute;
+        var routeInfo = opt.routes.filter(function(e){
+            return e.path === route;
+        })[0]
+        if(routeInfo){
+    
+            if(opt.trace) {history.pushState({}, '', '#'+routeInfo.path)}
+            
+            arrayViews.forEach(function(e){
+                if (e.getAttribute('data-route-view') === routeInfo.name){
+                    //accion si existe la ruta
+                    MostrarVista([e], arrayViews)
+                }
+            })
+             
+        }
+    }
+    function bindEvent(){
+        activeRoutes.forEach(function(e){
+            e.addEventListener('click', paraEvento, false);
+        })
+    }
+    
 
-var myFirstRouter = new Router('myFirstRouter', [
+
+
+
+
+
+    return {
+        init: "",
+        Router: Router,
+        setRoutes: setRoutes,
+        getRoutes: getRoutes,
+        goTo: goTo
+    }
+})()
+
+
+
+
+var myFirstRouter = new navigate.Router([
     {
         path: '/',
         name: 'Root'
@@ -22,61 +94,19 @@ var myFirstRouter = new Router('myFirstRouter', [
 ])
 
 if (window.location.hash === '') window.location.hash='#/'
-function extractRoute(route){
-    return route.slice(route.indexOf('#')+1)
-}
-//var currentPath = extractRoute(window.location.hash);
 
-var views = document.querySelectorAll('[data-route-view]')
-var activeRoutes = document.querySelectorAll('[data-route]')
-var arrayViews = [];
-for (var i = 0, len = views.length ; i < len ; i++){
-    arrayViews.push(views[i])
-}
-function pararaEvento(e){
-    navigate(e.target.getAttribute('data-route'))
-}
-function navigate(toRoute, opt){
-    var opt = opt || {};
-    opt.trace = typeof opt.trace === "undefined" ? true : opt.trace
-    var route = toRoute;
-    var routeInfo = myFirstRouter.routes.filter(function(e){
-        return e.path === route;
-    })[0]
-    if(routeInfo){
-
-        if(opt.trace) {history.pushState({}, '', '#'+routeInfo.path)}
-        
-        arrayViews.forEach(function(e){
-            if (e.getAttribute('data-route-view') === routeInfo.name){
-                //accion si existe la ruta
-                MostrarVista([e], arrayViews)
-            }
-        })
-         
-    }
-    console.log("route",route, routeInfo, opt.trace)
-
-}
 
 window.onload = function(){
 
-
- 
-    //events
-    activeRoutes.forEach(function(e){
-        e.addEventListener('click', pararaEvento, false);
-    })
-
- 
-
-
-    navigate(extractRoute(window.location.hash), {trace: false})
-    console.log("onLoad")
-    
+   navigate.goTo(extractRoute(window.location.hash), {trace: false})
 
 }
 
 window.onhashchange = function(){
-    navigate(extractRoute(window.location.hash), {trace: false})
+
+    navigate.goTo(extractRoute(window.location.hash), {trace: false})
+
 }
+
+
+
