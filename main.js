3249143257715a02af805ea6c4ead303b9a1d663
Tony Duran <yoet92@gmail.com>
2019-08-30 00:24:27 -0300

    modulo casi

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 var navigate = (function(){
+    'use strict';
 
+    var routes = new Object;
     function setRoutes(arrObj){
-        this.routes = new Router(arrObj);
+        console.log('setRoute')
+        routes.routes = arrObj;
     }
     function getRoutes(){
-        return this.routes;
+        console.log('getRoute')
+        return routes;
     }
-    var Router = function(paramRouter){
-        return {
-            routes: paramRouter
-        }
-    }
-    function extractRoute(route){
-        return route.slice(route.indexOf('#')+1);
+    function extractRoute(routeparam){
+        console.log('extractRoute')
+        return routeparam.slice(routeparam.indexOf('#')+1);
     }
     function getViews(){
+        console.log('get View')
         var views = document.querySelectorAll('[data-route-view]');
         var arrayViews = [];
         for (var i = 0, len = views.length ; i < len ; i++){
@@ -23,60 +24,74 @@ var navigate = (function(){
         return arrayViews;
     }
     function getActiveRoutes(){
+        console.log('get active rouutes')
         var activeRoutes = document.querySelectorAll('[data-route]');
         var arrayActiveRoutes = [];
         for (var i = 0, len = activeRoutes.length ; i < len ; i++){
-            arrayActiveRoutes.push(views[i])
+            arrayActiveRoutes.push(activeRoutes[i])
         }
         return arrayActiveRoutes;
     }
     function paraEvento(e){
+        console.log('para evento')
         goTo(e.target.getAttribute('data-route'))
     }
+    function bindEvent(){
+        getActiveRoutes().forEach(function(e){
+            e.removeEventListener('click', paraEvento, false);
+            e.addEventListener('click', paraEvento, false);
+            console.log('bindEvent')
+        })
+    }
+
     function goTo(toRoute, opt){
-        var opt = opt || {};
-        opt.trace = typeof opt.trace === "undefined" ? true : opt.trace
-        var route = toRoute;
-        var routeInfo = getRoutes().routes.filter(function(e){
-            return e.path === route;
-        })[0]
+        console.log('goTo..')
+        var opt = opt || new Object;
+        opt.trace = typeof opt.trace === "undefined" ? true : opt.trace;
+        var routeInfo = false;
+        if(typeof getRoutes().routes !== "undefined"){
+            routeInfo = getRoutes().routes.filter(function(e){
+                return e.path === toRoute; 
+            })[0]
+        } 
+        console.log(routeInfo)
         if(routeInfo){
-    
             if(opt.trace) {history.pushState({}, '', '#'+routeInfo.path)}
             
             getViews().forEach(function(e){
                 if (e.getAttribute('data-route-view') === routeInfo.name){
                     //accion si existe la ruta
                     // MostrarVista([e], arrayViews)
-                    console.log(routeInfo.name);
+                    console.log("dentro Get View");
                 }
             })
-             
+            
+            console.log("dentro goTo IF")
         }
+        console.log('goTo _toRoute', toRoute, opt.trace)
     }
-    function bindEvent(){
-        getActiveRoutes().forEach(function(e){
-            e.addEventListener('click', paraEvento, false);
-        })
-    }
+
     
 
     return {
         init: function(){
+            console.log('init')
             bindEvent()
         },
-        Router: Router,
         setRoutes: setRoutes,
         getRoutes: getRoutes,
         goTo: goTo,
-        extractRoute: extractRoute
+        extractRoute: extractRoute,
+        bindEvent: bindEvent,
+        getActiveRoutes: getActiveRoutes,
+        getViews: getViews
     }
 })()
 
 
 
 
-var myFirstRouter = new navigate.Router([
+navigate.setRoutes([
     {
         path: '/',
         name: 'Root'
