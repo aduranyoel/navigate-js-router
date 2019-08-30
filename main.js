3e0adf347c0eceeaedc826c6703e05af101f7da5
Tony Duran <yoet92@gmail.com>
2019-08-30 12:28:36 -0300

    navigate complete

--- main.js
+++ main.js
@@ -1,125 +1,26 @@
-var navigate = (function(){
-    'use strict';
-
-    var routes = new Object;
-    function setRoutes(arrObj){
-        console.log('setRoute')
-        routes.routes = arrObj;
-    }
-    function getRoutes(){
-        console.log('getRoute')
-        return routes;
-    }
-    function extractRoute(routeparam){
-        console.log('extractRoute')
-        return routeparam.slice(routeparam.indexOf('#')+1);
-    }
-    function getViews(){
-        console.log('get View')
-        var views = document.querySelectorAll('[data-route-view]');
-        var arrayViews = [];
-        for (var i = 0, len = views.length ; i < len ; i++){
-            arrayViews.push(views[i])
-        }
-        return arrayViews;
-    }
-    function getActiveRoutes(){
-        console.log('get active rouutes')
-        var activeRoutes = document.querySelectorAll('[data-route]');
-        var arrayActiveRoutes = [];
-        for (var i = 0, len = activeRoutes.length ; i < len ; i++){
-            arrayActiveRoutes.push(activeRoutes[i])
-        }
-        return arrayActiveRoutes;
-    }
-    function paraEvento(e){
-        console.log('para evento')
-        goTo(e.target.getAttribute('data-route'))
-    }
-    function bindEvent(){
-        getActiveRoutes().forEach(function(e){
-            e.removeEventListener('click', paraEvento, false);
-            e.addEventListener('click', paraEvento, false);
-            console.log('bindEvent')
-        })
-    }
-
-    function goTo(toRoute, opt){
-        console.log('goTo..')
-        var opt = opt || new Object;
-        opt.trace = typeof opt.trace === "undefined" ? true : opt.trace;
-        var routeInfo = false;
-        if(typeof getRoutes().routes !== "undefined"){
-            routeInfo = getRoutes().routes.filter(function(e){
-                return e.path === toRoute; 
-            })[0]
-        } 
-        console.log(routeInfo)
-        if(routeInfo){
-            if(opt.trace) {history.pushState({}, '', '#'+routeInfo.path)}
-            
-            getViews().forEach(function(e){
-                if (e.getAttribute('data-route-view') === routeInfo.name){
-                    //accion si existe la ruta
-                    // MostrarVista([e], arrayViews)
-                    console.log("dentro Get View");
-                }
-            })
-            
-            console.log("dentro goTo IF")
-        }
-        console.log('goTo _toRoute', toRoute, opt.trace)
-    }
-
-    
-
-    return {
-        init: function(){
-            console.log('init')
-            bindEvent()
-        },
-        setRoutes: setRoutes,
-        getRoutes: getRoutes,
-        goTo: goTo,
-        extractRoute: extractRoute,
-        bindEvent: bindEvent,
-        getActiveRoutes: getActiveRoutes,
-        getViews: getViews
-    }
-})()
-
-
-
-
 navigate.setRoutes([
     {
         path: '/',
-        name: 'Root'
+        view: 'Root',
+        show: true,
+        before: function(){
+            console.log("Before Action")
+        },
+        after: function(){
+            console.log("After Action")
+        }
     },
     {
         path: '/contacts',
-        name: 'Contacts'
+        view: 'Contacts'
     },
     {
         path: '/about',
-        name: 'About'
+        view: 'About'
     }
-])
+]);
 
-if (window.location.hash === '') window.location.hash='#/'
-
-
-window.onload = function(){
-
-   navigate.goTo(navigate.extractRoute(window.location.hash), {trace: false})
-
-}
-
-window.onhashchange = function(){
-
-    navigate.goTo(navigate.extractRoute(window.location.hash), {trace: false})
-
-}
+navigate.init();
 
 
 
---
+++ navigate.js
@@ -0,0 +1,130 @@
+var navigate = (function ($w) {
+    'use strict';
+
+    var routes = new Object;
+    routes.routes = [];
+    function setRoutes(arrObj) {
+        routes.routes = arrObj;
+    }
+    function getRoutes() {
+        return routes;
+    }
+    function extractRoute(routeparam) {
+        return routeparam.slice(routeparam.indexOf('#') + 1);
+    }
+    function getViews() {
+        var views = document.querySelectorAll('[data-route-view]');
+        var arrayViews = [];
+        for (var i = 0, len = views.length; i < len; i++) {
+            arrayViews.push(views[i]);
+        }
+        return arrayViews;
+    }
+    function getActiveRoutes() {
+        var activeRoutes = document.querySelectorAll('[data-route]');
+        var arrayActiveRoutes = [];
+        for (var i = 0, len = activeRoutes.length; i < len; i++) {
+            arrayActiveRoutes.push(activeRoutes[i]);
+        }
+        return arrayActiveRoutes;
+    }
+    function paraEvento(e) {
+        goTo(e.target.getAttribute('data-route'));
+    }
+    function bindEvent() {
+        getActiveRoutes().forEach(function (e) {
+            e.removeEventListener('click', paraEvento, false);
+            e.addEventListener('click', paraEvento, false);
+        });
+    }
+    function putNavigate() {
+        goTo(extractRoute($w.location.hash), { trace: false });
+    }
+    function addOnLoad() {
+        $w.removeEventListener('load', putNavigate, false);
+        $w.addEventListener('load', putNavigate, false);
+    }
+    function addOnHashchange() {
+        $w.removeEventListener('hashchange', putNavigate, false);
+        $w.addEventListener('hashchange', putNavigate, false);
+    }
+    function begin() {
+        if ($w.location.hash === '') $w.location.hash = '#/';
+    }
+    function hideViews() {
+        getViews().forEach(function (e) {
+            e.style.display = 'none';
+        });
+    }
+    function mostrarVista(mostrar, ocultar) {
+        function hide(fn) {
+            ocultar.forEach(function (e) {
+                e.style.display = 'none';
+            });
+            fn();
+        }
+        function show() {
+            mostrar.forEach(function (e) {
+                e.style.display = 'block';
+            });
+        }
+        hide(show);
+    }
+    function extendObj() {
+        for (var i = 1; i < arguments.length; i++)
+            for (var key in arguments[i])
+                if (arguments[i].hasOwnProperty(key))
+                    arguments[0][key] = arguments[i][key];
+        return arguments[0];
+    }
+    function goTo(toRoute, opt) {
+        var routeInfo = false;
+        if (getRoutes().routes.length > 0) {
+            routeInfo = getRoutes().routes.filter(function (e) {
+                return e.path === toRoute;
+            })[0];
+            if (typeof routeInfo === "undefined") routeInfo = false;
+        }
+        var defaults = {
+            trace: true,
+            show: true,
+            before: new Function,
+            after: new Function
+        };
+        opt = opt || new Object;
+        var settings = extendObj({}, defaults, routeInfo, opt);
+        if (routeInfo) {
+            settings.before();
+            if (settings.trace) { history.pushState({}, '', '#' + routeInfo.path);}
+            if (settings.show) {
+                getViews().forEach(function (e, i, all) {
+                    if (e.getAttribute('data-route-view') === routeInfo.view) {
+                        mostrarVista([e], all);
+                    }
+                });
+            }
+            settings.after();
+        }
+    }
+
+    return {
+        init: function () {
+            addOnLoad();
+            addOnHashchange();
+            hideViews();
+            begin();
+            bindEvent();
+        },
+        goTo: goTo,
+        begin: begin,
+        getViews: getViews,
+        setRoutes: setRoutes,
+        getRoutes: getRoutes,
+        bindEvent: bindEvent,
+        hideViews: hideViews,
+        addOnLoad: addOnLoad,
+        extractRoute: extractRoute,
+        addOnHashchange: addOnHashchange,
+        getActiveRoutes: getActiveRoutes
+    };
+})(window)
